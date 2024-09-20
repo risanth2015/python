04584362426c8351be68af3eb486dce3c524d3db
risanth2015 <arthi.becse@gmail.com>
2024-09-20 17:12:46 +0800

    date time

---
+++ main.py
@@ -0,0 +1,6 @@
+from datetime import date,time,datetime
+today=date.today()
+now=datetime.now()
+print("todays's date is ", today)
+print("nom the time is  ", now)
+print(today.year,today.month,today.day)
